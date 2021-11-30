b02fcb43687de41ffa7fdaa20808df0779a9b673
PatrickVuscan <patrick.vuscan@gmail.com>
2021-11-30 10:33:34 -0500

    +Comments and detailed error messages in Quest API

--- server/src/controllers/Questionnaire.ts
+++ server/src/controllers/Questionnaire.ts
@@ -22,11 +22,11 @@ const getQuestionnaire = async (req: Request, res: Response) => {
   } catch (err: any) {
     if (err.response.status === 404) {
       return res.status(404).json({
-        error: 'ID was not found',
+        error: 'Questionnaire with given ID was not found',
       });
     }
     return res.status(400).json({
-      error: `Failed to get questionnaire with id ${id}`,
+      error: `Failed to get questionnaire with ID ${id}`,
     });
   }
 };
@@ -34,28 +34,31 @@ const getQuestionnaire = async (req: Request, res: Response) => {
 const postQuestionnaire = async (req: Request, res: Response) => {
   const { id } = req.params;
   let questionnaireData;
+
+  // Check if the questionnaire can be found
   try {
     questionnaireData = await fhirClient.read({ resourceType: 'Questionnaire', id });
   } catch (err: any) {
     if (err.response.status === 404) {
       return res.status(404).json({
-        error: 'ID was not found',
+        error: 'ID given was not found',
       });
     }
     return res.status(400).json({
-      error: `Failed to get questionnaire with id ${id}`,
+      error: `Failed to get questionnaire with ID ${id}`,
     });
   }
 
+  // Pass the questionnaire data through validation, check for errors
   const validationErrors = validateQuestionnaire(questionnaireData.item, req.body);
   if (Object.keys(validationErrors).length > 0) {
-    console.log(validationErrors);
-    const resi = res.status(400).json(validationErrors);
-    return resi;
+    console.log(`Validation Errors for questionnaire with id=${id}: ${validationErrors}`);
+    return res.status(400).json(validationErrors);
   }
 
   const questionnaireResponse = constructResponse(questionnaireData, req.body);
 
+  // Pass the questionnaire response through validation, on the FHIR API side
   let validateResponse = { data: {} };
   try {
     validateResponse = await axios.post(
@@ -66,6 +69,7 @@ const postQuestionnaire = async (req: Request, res: Response) => {
     return res.status(400).json(validateResponse.data);
   }
 
+  // Once validated twice, submit the response
   let submitResponse = { data: {} };
   try {
     submitResponse = await axios.post(`${process.env.FHIR_API}/QuestionnaireResponse`, questionnaireResponse);
@@ -76,10 +80,15 @@ const postQuestionnaire = async (req: Request, res: Response) => {
   return res.status(200).json(submitResponse.data);
 };
 
+// Questionnaire Paging API, which uses the pevious/next strings created by the
+// FHIR library to get the next set of questionnaires, or defaults to returning
+// a first page of questionnaires
 type pagingReqQuery = { next: string; previous: string };
 const getQuestionnairesPage = async (req: Request<pagingReqQuery>, res: Response) => {
   const { next, previous } = req.query;
+
   try {
+    // Ensure string types of previous/next
     if ((next && typeof next !== 'string') || (previous && typeof previous !== 'string')) {
       return res.status(400).json({
         error: `Failed to get paged questionnaires because next/previous is not a string`,
@@ -96,6 +105,7 @@ const getQuestionnairesPage = async (req: Request<pagingReqQuery>, res: Response
       return res.json({ response: response.data });
     }
 
+    // If previous/next are not passed in, do default first paging request
     const response = await fhirClient.search({ resourceType: 'Questionnaire', searchParams: { _count: 10 } });
     return res.json({ response });
   } catch (err: any) {

44c4b973c7b50bb88138d05b495170a3fa3c199a
PatrickVuscan <patrick.vuscan@gmail.com>
2021-11-30 11:15:21 -0500

    Added comments/definitions to Questionnaire lib

--- server/src/lib/Questionnaire.ts
+++ server/src/lib/Questionnaire.ts
@@ -1,50 +1,59 @@
 const { sanitizeUrl } = require('@braintree/sanitize-url');
 
+const safeStringRegx = /[ \r\n\t\S]+/;
+const dateRegx = /([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1]))?)?/;
+const dateTimeRegx =
+  /([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1])(T([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]+)?(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00)))?)?)?/;
+const timeRegx = /([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]+)?/;
+
+// Method to validate questionnaire item, based on it's type, and other
+// properties on the questionnaire item
 const validateQuestion = (question: any, values: any) => {
-  
   const errors: any = {};
+
   if (
     question.required &&
     (!(question.linkId in values) || values[question.linkId] === '' || values[question.linkId] === undefined)
   ) {
-    errors[question.linkId] = 'This field is required';
+    // required
+    errors[question.linkId] = 'This field requires a non-empty answer.';
   } else if ('maxLength' in question && values[question.linkId].length > question.maxLength) {
     // maxlength
     errors[question.linkId] = `This field is over max length of ${question.maxLength}`;
   } else if (question.type === 'display' && values[question.linkId]) {
     // display
-    errors[question.linkId] = `This field should not have an answer...`;
+    errors[question.linkId] = `This field should not have an answer.`;
   } else if (question.type === 'boolean' && values[question.linkId] !== 'true' && values[question.linkId] !== 'false') {
     // boolean
-    errors[question.linkId] = `This field is not a boolean`;
-  } else if ((question.type === 'decimal' || question.type === 'quantity') && typeof values[question.linkId] !== 'number') {
-    // decimal
-    errors[question.linkId] = `This field is not a decimal`;
+    errors[question.linkId] = `This field requires a boolean answer.`;
+  } else if (
+    (question.type === 'decimal' || question.type === 'quantity') &&
+    typeof values[question.linkId] !== 'number'
+  ) {
+    // decimal or quantity
+    errors[question.linkId] = `This field is not a decimal.`;
   } else if (
     question.type === 'integer' &&
     (typeof values[question.linkId] !== 'number' || values[question.linkId] % 1 !== 0)
   ) {
     // integer
-    errors[question.linkId] = `This field is not an integer`;
+    errors[question.linkId] = `This field is not an integer.`;
   } else if (
     (question.type === 'string' || question.type === 'text') &&
     (typeof values[question.linkId] !== 'string' ||
-      /[ \r\n\t\S]+/.test(values[question.linkId]) === false ||
-      values[question.linkId].length >= 1048576 - 1) // This verifies string is < 1MB, including terminating char?
+      safeStringRegx.test(values[question.linkId]) === false || // This part ensures no weird string inputs
+      values[question.linkId].length >= 1048576 - 1) // I believeThis verifies string is < 1MB, including terminating character
   ) {
     // string or text
     errors[
       question.linkId
-    ] = `This field is not a string containing only accepted unicode characters, and below 1MB in size`;
+    ] = `This field is not a string containing only accepted unicode characters, and below 1MB in size.`;
   } else if (question.type === 'url' && sanitizeUrl(values[question.linkId]) !== values[question.linkId]) {
     // url
-    errors[question.linkId] = `This field is not a valid/safe url`;
+    errors[question.linkId] = `This field is not a valid/safe url.`;
   } else if (
     question.type === 'date' &&
-    (/([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1]))?)?/.test(
-      values[question.linkId],
-    ) === false ||
-      Number.isNaN(Date.parse(values[question.linkId])) === true)
+    (dateRegx.test(values[question.linkId]) === false || Number.isNaN(Date.parse(values[question.linkId])) === true)
   ) {
     // date
     errors[
@@ -52,11 +61,7 @@ const validateQuestion = (question: any, values: any) => {
     ] = `This field is not a valid date of the format YYYY, YYYY-MM, or YYYY-MM-DD. No timezone should be added.`;
   } else if (question.type === 'dateTime' && Number.isNaN(Date.parse(values[question.linkId])) === true) {
     // dateTime
-    if (
-      /([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1])(T([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]+)?(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00)))?)?)?/.test(
-        values[question.linkId],
-      ) === false
-    ) {
+    if (dateTimeRegx.test(values[question.linkId]) === false) {
       // ! TODO: Reassign the value here, if it's missing certain aspects but it otherwise is a valid datetime
       try {
         // values[question.linkId] = (new Date(values[question.linkId])).toLocaleString();
@@ -72,8 +77,7 @@ const validateQuestion = (question: any, values: any) => {
     }
   } else if (
     question.type === 'time' &&
-    (/([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]+)?/.test(values[question.linkId]) === false ||
-      Number.isNaN(Date.parse(values[question.linkId])) === true)
+    (timeRegx.test(values[question.linkId]) === false || Number.isNaN(Date.parse(values[question.linkId])) === true)
   ) {
     // time
     errors[question.linkId] = `This field is not a valid time of the format hh:mm:ss. No timezone should be added.`;
@@ -110,13 +114,16 @@ const validateQuestion = (question: any, values: any) => {
     }
   }
 
-  // Have not implemented choice - too weird
-  // enable whens - question has no data when not enabled
-  // ^ see components/formpage/enableWhen -> i did some weird shit to make sure they dont show up when not enabled (might be helpful)
+  // ! TODO: "enableWhen" not implemented - question should have no data when not enabled
+  // See ~/app/src/components/formpage/enableWhen.tsx for example of interaction with
+  // "enableWhen" in frontend, for guidance on how to implement this
 
   return errors;
 };
 
+// Validates a questionnaire, by going through all items, validating them one
+// at a time. If a questionnaire item contains group items itself, it recurses through
+// those items as well, to ensure the entire questionnaire is validated
 const validateQuestionnaire = (questionnaireItems: any, values: any) => {
   let errors: any = {};
   const questionStack = [];
@@ -125,18 +132,20 @@ const validateQuestionnaire = (questionnaireItems: any, values: any) => {
   while (questionStack.length > 0) {
     const questionnaireItem: any = questionStack.pop();
 
+    // Check if there are things to validate
     for (let i = 0; i < questionnaireItem.length; i += 1) {
       const item = questionnaireItem[i];
 
+      // If an item is actually a group, add it to the stack and move on
       if (item.type === 'group') {
         questionStack.push(item.item);
-      } else {
-        if (item.linkId in values) {
-          errors = { ...validateQuestion(item, values), ...errors };
+      } else if (item.linkId in values) {
+        // Validate the question, retrieve any errors that may exist
+        errors = { ...validateQuestion(item, values), ...errors };
 
-          if ('item' in item) {
-            questionStack.push(item.item);
-          }
+        // Handle recurrsion
+        if ('item' in item) {
+          questionStack.push(item.item);
         }
       }
     }
@@ -152,8 +161,6 @@ const constructResponse = (questionnaireData: any, values: any) => {
     item: questionnaireData.item,
     status: 'completed',
   };
-
-  response.item = questionnaireData.item;
 
   const questionStack = [];
   questionStack.push(response.item);
@@ -234,6 +241,7 @@ const constructResponse = (questionnaireData: any, values: any) => {
           questionStack.push(item.item);
         }
       }
+
       const validKeys = ['linkId', 'answer', 'item'];
       Object.keys(item).forEach((key) => validKeys.includes(key) || delete item[key]);
       if (item.answer && item.answer.length === 0) {

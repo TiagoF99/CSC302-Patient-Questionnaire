d5d0fae36908795e4dc12e5e4b02a62e2ede32ce
Rishab Luthra <rishabjsluthra@gmail.com>
2021-12-01 16:28:51 -0500

    init backend testing

---
+++ server/src/test/app.test.ts
@@ -0,0 +1,14 @@
+import request from 'supertest';
+import app from '../app';
+
+describe('Check status endpoint', () => {
+  jest.setTimeout(30000);
+  // @ts-ignore
+  test('Status 200', async () => {
+    await request(app)
+      .get('/api/status')
+      .then(async (response) => {
+        expect(response.statusCode).toBe(200);
+      });
+  });
+});

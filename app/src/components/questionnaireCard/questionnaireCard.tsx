b945640b74ed17f2d1be82471a563505e0c62058
Kevin Ge <kevingehd@gmail.com>
2021-12-01 17:24:24 -0500

    added version to questionnaire cards

--- app/src/components/questionnaireCard/questionnaireCard.tsx
+++ app/src/components/questionnaireCard/questionnaireCard.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import questionnaireCard from './questionnaireCard.module.css';
 
 
-const QuestionnaireCard = ({ title, desc, date, id }) => {
+const QuestionnaireCard = ({ title, desc, date, id, version }) => {
   return (
     <div className={questionnaireCard.card}>
-      <strong>{title ? title: "No title provided"}</strong> <br/>
+      <strong>{title ? title : "No title provided"} {version ? version : ""}</strong> <br/>
       {desc ? desc : "No description provided"} <br/>
       {date ? date.substring(0, Math.min(10, date.length)) : "No date provided"} <br/>
       id: {id}

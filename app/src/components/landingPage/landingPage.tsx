ba92570f9e2faccf1582232bd7cf56aa32c0a9cf
Kevin Ge <kevingehd@gmail.com>
2021-12-03 17:11:52 -0500

    landing page id input box

--- app/src/components/landingPage/landingPage.tsx
+++ app/src/components/landingPage/landingPage.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Formik, Field, Form } from 'formik';
+import { useHistory } from 'react-router-dom';
 import QuestionnaireList from '../questionnaireList/questionnaireList';
 
+
 const LandingPage = () => {
+  const history = useHistory();
+
   return (
-    <div>
+      <div>
+        <h1>Access questionnaire by ID</h1>
+        <div>
+        <Formik
+          initialValues={{qid: ''}}
+          
+          onSubmit={(values, actions) => {
+            console.log(JSON.stringify(values, null, 2)); // for debugging
+            history.push({
+              pathname: `/form`,
+              search: `?id=${values.qid}`,
+            });
+          }}
+        >
+          <Form>
+            <Field name="qid"/>
+            <button type="submit">Submit</button>
+          </Form>
+        </Formik>
+      </div>
+      <h3>Or..</h3>
       <h1>Select a questionnaire</h1>
       <QuestionnaireList/>
     </div>
